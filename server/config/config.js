1a77c488c57c01ae3491d30cad8f9bed82dfbd3e
gokemon <michael.of.gokey@gmail.com>
2017-03-09 22:49:53 -0500

    setup express app

---
+++ server/config/config.js
@@ -0,0 +1,8 @@
+// Database URL
+module.exports = {
+    // local
+    'url' : 'localhost/conferenceDB'
+
+// Uncomment to connect with MongoDB on Cloud
+// 'url' : 'mongodb://username:password@kahana.mongohq.com:10073/node-api'
+};
---
+++ server/routes/index.js
@@ -0,0 +1,12 @@
+// index router page
+var express = require('express');
+var router = express.Router();
+
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'conference-api' });
+});
+
+
+module.exports = router;
